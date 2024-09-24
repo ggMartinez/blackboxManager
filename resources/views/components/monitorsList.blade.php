ee81be8e0582ba7d68a2dd71b98d6b4e73329d54
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2024-09-24 18:02:11 -0300

    Mis tocs

--- resources/views/components/monitorsList.blade.php
+++ resources/views/components/monitorsList.blade.php
@@ -3,27 +3,29 @@
     @if($categorizedMonitors === null || count($categorizedMonitors) === 0)
         <br>
         <h3>No monitors found.</h3>
-    @else 
+    @else
 
         @foreach($categorizedMonitors as $category)
             <h4>{{ $category['category'] }}</h4>
             <table class="table table-striped table-hover">
-                <thead> 
+                <thead>
                     <tr>
                         <th>Name</th>
                         <th>URL</th>
                         <th>Category</th>
                         <th>Added Date</th>
+                        <th>Updated Date</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
-                <tbody> 
+                <tbody>
                     @foreach($category['sites'] as $site)
                         <tr>
                             <td>{{ $site->name }}</td>
                             <td>{{ $site->url }}</td>
                             <td>{{ $site->category }}</td>
                             <td>{{ $site->created_at->format('d/m/Y') }}</td>
+                            <td>{{ $site->updated_at->format('d/m/Y') }}</td>
                             <td>
                                 <button onClick="showUpdateModal('{{ $site->id }}','{{ $site->name }}','{{ $site->url }}','{{ $site->category }}')" class="btn btn-warning"><i class="bi bi-pencil"></i></button>
                                 <a href="/monitor/{{ $site->id }}/delete" class="btn btn-danger"><i class="bi bi-trash"></i></a>
@@ -35,5 +37,5 @@
             <br> <br>
             <hr class="bg border-2 border-top border" />
 
-        @endforeach        
+        @endforeach
     @endif
